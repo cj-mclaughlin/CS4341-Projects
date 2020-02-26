40f078aaf81fb2d1659b868a62a2835191f0295a
Vlad-Stelea <vladstelea@gmail.com>
2020-02-26 12:41:40 -0500

    Added interactive character

--- Bomberman/groupNN/scenario1/variant1.py
+++ Bomberman/groupNN/scenario1/variant1.py
@@ -9,13 +9,13 @@ from game import Game
 # TODO This is your code!
 sys.path.insert(1, '../groupNN')
 from testcharacter import TestCharacter
-
+from interactivecharacter import InteractiveCharacter
 
 # Create the game
 g = Game.fromfile('map.txt')
 
 # TODO Add your character
-g.add_character(TestCharacter("me", # name
+g.add_character(InteractiveCharacter("me", # name
                               "C",  # avatar
                               0, 0  # position
 ))

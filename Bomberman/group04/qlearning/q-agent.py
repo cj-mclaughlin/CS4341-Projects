886342430012fc9f2d4070e7e02a0fcf20c1f71c
Connor McLaughlin <cjmclaughlin@wpi.edu>
2020-02-28 15:54:16 -0500

    init class

---
+++ Bomberman/group04/qlearning/q-agent.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.insert(0, '../../bomberman')
+sys.path.insert(1, '..')
+
+from entity import CharacterEntity
+
+
+# TODO implement
+class QAgent(CharacterEntity):
+    pass
+
+if __name__ == "__main__":
+    print("Imports passed")
+    
+    

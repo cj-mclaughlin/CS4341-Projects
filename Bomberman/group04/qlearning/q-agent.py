a21446dad77ffc36d509291327e66232fe23b0a2
Vlad-Stelea <vladstelea@gmail.com>
2020-02-28 16:03:41 -0500

    Added basic structure for q-agent

--- Bomberman/group04/qlearning/q-agent.py
+++ Bomberman/group04/qlearning/q-agent.py
@@ -3,11 +3,17 @@ sys.path.insert(0, '../../bomberman')
 sys.path.insert(1, '..')
 
 from entity import CharacterEntity
-
+from actions import Action
 
 # TODO implement
 class QAgent(CharacterEntity):
-    pass
+    def __init__(self):
+        self.weights = []
+        self.feature_functions = []
+
+    # TODO evaluate the quality of the current move using the feature functions and weights
+    def evaluate_move(self, action):
+        pass
 
 if __name__ == "__main__":
     print("Imports passed")

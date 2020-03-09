d983cb830be3523b9dcae4e1a9b61bb0cf0e0d52
wblucca <wblucca@wpi.edu>
2020-03-08 22:37:19 -0400

    Clean up featurefunctions

--- Bomberman/group04/qlearning/featurefunctions.py
+++ Bomberman/group04/qlearning/featurefunctions.py
@@ -11,8 +11,6 @@ from queue import PriorityQueue
 import numpy as np
 
 
-# TODO feature functions for small boards lol
-
 # Feature based on distance to nearest monster
 # PARAM[SensedWorld] state: the current state of the map
 # PARAM[Action] action: the action to evaluate
@@ -31,8 +29,8 @@ def dist_to_monster(state, action, character):
                 shortest_path = path
                 shortest_len = len(path)
 
+    # No monsters
     if shortest_path is None:
-        # No monsters
         return 0
     
     # Feature is inversely proportional to distance
@@ -67,28 +65,25 @@ def monster_threat(state, action, character):
     
     normal_exit_vec, normal_monster_vec = [], []
     
-    # try normalizing vector before
+    # Normalize vector
     exit_vec_length = math.sqrt(exit_vec[0]**2 + exit_vec[1]**2)
     if exit_vec_length == 0:
         normal_exit_vec = [0,0]
     else:
         normal_exit_vec = [vector/exit_vec_length for vector in exit_vec]
-    #print(normal_exit_vec)
 
     max_threat = -1
     for monsterlist in state.monsters.values():
         for monster in monsterlist:
             monster_vec = [monster.x - new_x, monster.y - new_y] # old way
             
-            # try normalizing vector before
+            # Normalize vector
             monster_vec_length = math.sqrt(monster_vec[0]**2 + monster_vec[1]**2)
             if monster_vec_length == 0:
                 normal_monster_vec = [0,0]
             else:
                 normal_monster_vec = [vector/monster_vec_length for vector in monster_vec]
-            #print(normal_monster_vec)
             
-            #threat = dotp(exit_vec, monster_vec) # old way
             threat = np.dot(normal_exit_vec, normal_monster_vec)
             threat = threat ** 3
             if threat > max_threat:
@@ -118,31 +113,31 @@ def bomb_danger_zone(state, action, character):
         ticking_bomb = True
 
 
-    # checks for active bomb
+    # Checks for active bomb
     if (ticking_bomb):
-        # check if we are in the x component of explosion
+        # Check if we are in the x component of explosion
         if (abs(new_x - bomb.x) <= 4 and new_y == bomb.y):
             in_explosion_radius = True
-        # y component
+        # Check y component
         elif (abs(new_y - bomb.y) <= 4 and new_x == bomb.x):
             in_explosion_radius = True
     
-    # checks for active explosions
+    # Checks for active explosions
     if (state.explosion_at(new_x, new_y)):
         # really bad
         return 1
     
     if (in_explosion_radius):
-        # more bad depending on how close bomb is to going off
+        # Only bad if bomb is close to going off
         if bomb.timer <= 1:
             return 1
         return 0
     
-    # no active bombs or explosion on new tile
+    # No active bombs or explosion on new tile
     return 0
 
 
-# Feature for detecting if a path to the exit exists
+# Feature for detecting if a path to the exit exists, or is about to be blown open
 # PARAM[SensedWorld] state: the current state of the map
 # PARAM[Action] action: the action to evaluate
 # PARAM[MovableEntity] character: the bomberman character this is evaluating for
@@ -158,56 +153,23 @@ def no_path_bomb(state, action, character):
                 if 0 <= expl_x < state.width() and 0 <= expl_y < state.height() and state.wall_at(expl_x, expl_y):
                     # Wall found in south direction
                     return 1
+        # Not placing bomb
         return 0
     
     # Path found
     return 0
 
 
-# BFS for optimal path
-# PARAM[SensedWorld] state: the current state of the board
-# PARAM[tuple(int, int)] start: starting coordinates
-# PARAM[tuple(int, int)] goal: goal coordinates
-def bfs(state, start, goal):
-    queue = collections.deque([[start]])
-    seen = set([start])
-
-    # perform bfs, leaving us with path to exit
-    exit_found = False
-    while queue:
-        path = queue.popleft()
-        x, y = path[-1]
-
-        # check if we have reached exit
-        if x == goal[0] and y == goal[1]:
-            exit_found = True
-            break
-
-        # Add neighbors to queue
-        for x2, y2 in ((x+1,y), (x-1,y), (x,y+1), (x,y-1), (x+1, y+1), (x+1, y-1), (x-1, y-1), (x-1, y+1)):
-            if 0 <= x2 < state.width() and 0 <= y2 < state.height() and not state.wall_at(x2, y2) and (x2, y2) not in seen:
-                queue.append(path + [(x2, y2)])
-                seen.add((x2, y2))
-
-    if exit_found:
-        # TODO what should this be
-        return path
-        #return path[1][0] - start[0], path[1][1] - start[1]
-    
-    # No path
-    return None
-
-
+# Finds an optimal path from start to goal using the A* algorithm
+# PARAM[SensedWorld] state: the current state of the map
+# PARAM[tuple(int, int)] start: the starting coordinates
+# PARAM[tuple(int, int)] goal: the goal coordinates
 def A_star(state, start, goal):
+    """Performs A* search to find a path from start to goal"""
     def g(state, pos):
         return 11 if(state.wall_at(*pos)) else 1
     def h(state, pos):
         return tile_dist(*pos, *goal)
-    
-    # @dataclass(order = True)
-    # class prioritized_item:
-    #     priority: int
-    #     item: Any=field(compare=False)
 
     frontier = PriorityQueue()
     frontier.put((0, start))
@@ -220,13 +182,12 @@ def A_star(state, start, goal):
         priority_pair = frontier.get()
         current = priority_pair[1]
 
-        if(current == goal):
+        if (current == goal):
             break
 
         # Add neighbors to queue
         x,y = current[0], current[1]
         for next_pos in ((x+1,y), (x-1,y), (x,y+1), (x,y-1)):
-        # for next_pos in ((x+1,y), (x-1,y), (x,y+1), (x,y-1), (x+1, y+1), (x+1, y-1), (x-1, y-1), (x-1, y+1)):
             
             # Check that the position is valid
             if 0 <= next_pos[0] < state.width() and 0 <= next_pos[1] < state.height():
@@ -238,7 +199,7 @@ def A_star(state, start, goal):
                     frontier.put((priority, next_pos))
                     came_from[next_pos] = current
 
-    #Loop back and build the path
+    # Loop back and build the path
     cur_coor = current
     path = [cur_coor]
     while(cur_coor != None):
@@ -259,6 +220,8 @@ def tile_dist(x1, y1, x2, y2):
 
 
 # Returns true if one of the tiles in the given path is a wall
+# PARAM[SensedWorld] state: the current state of the map
+# PARAM[list(tuple(int, int))] path: the path to evaluate
 def wall_in_path(state, path):
     for loc in path:
         if loc is not None and state.wall_at(*loc):
@@ -294,24 +257,6 @@ def valid_location(state, x, y):
     if 0 <= x < state.width() and 0 <= y < state.height() and not state.wall_at(x, y):
         return True
     return False
-
-
-# Compute the dot product of two vectors
-# PARAM[tuple(int...)] v1: the first vector
-# PARAM[tuple(int...)] v2: the second vector
-def dotp(v1, v2):
-    """Return the dot product of the two vectors"""
-    mag1, mag2 = 0, 0
-    for i in range(len(v1)):
-        mag1 += v1[i] * v1[i]
-        mag2 += v2[i] * v2[i]
-    mag1 = math.sqrt(mag1)
-    mag2 = math.sqrt(mag2)
-
-    if mag1 == 0 or mag2 == 0:
-        return 0
-
-    return sum([(v1[i] / mag1) * (v2[i] / mag2) for i in range(len(v1))])
 
 
 feature_functions = [
